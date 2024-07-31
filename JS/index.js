23616573fc972ea2a1d610f99329602d118804af
Rik177 <mefodi12@gmail.com>
2024-08-01 01:53:54 +0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+// const button = document.getElementById('frbutton')
+
+// function setFocus() {
+//     button.focus();
+// }
+
+// window.onload = setFocus;
+
+
+// button.addEventListener('blur', setFocus);
+
+// const elemsSel = document.querySelectorAll('.reproductions__button');
+// console.log(elemsSel);
+
+// for (let value of elemsSel) { 
+//     console.log(value)
+// }
